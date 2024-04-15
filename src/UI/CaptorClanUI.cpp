12650075b98d6fb5394593129e81da42312ef9b5
Tatenshi <55283269+Tatenshi@users.noreply.github.com>
2024-04-15 18:24:17 +0200

    Fixed Clantag not showing

--- src/UI/CaptorClanUI.cpp
+++ src/UI/CaptorClanUI.cpp
@@ -130,7 +130,7 @@ namespace CaptorClanUI {
         clanImage = UIUtils::CreateRoundRectImage(mainPanel->get_transform(), {0, 0}, {8, 2.5});
         clanImage->set_material(BundleLoader::bundle->clanTagBackgroundMaterial);
 
-        clanTag = CreateText(clanImage->get_transform(), "", UnityEngine::Vector2(0.0, 0.0));
+        clanTag = CreateText(clanImage->get_transform(), "", {0, 0}, {30, 5});
         clanTag->set_enableAutoSizing(true);
         clanTag->set_fontSizeMin(0.1);
         clanTag->set_fontSizeMax(3.0);
