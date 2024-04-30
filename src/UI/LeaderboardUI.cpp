db42047684418097b32f25956020e461e6be8419
NSGolova <golova@golova.dev>
2024-04-30 21:33:47 +0100

    Fixed player info update on first log in

--- src/UI/LeaderboardUI.cpp
+++ src/UI/LeaderboardUI.cpp
@@ -233,8 +233,6 @@ namespace LeaderboardUI {
     }
 
     void updatePlayerInfoLabel() {
-        if (uploadStatus == NULL) return;
-        
         auto const& player = PlayerController::currentPlayer;
         if (player != std::nullopt) {
             if (!player->name.empty()) {
